17ae35c44d6b68749e23db76f36f81e422d5487c
Benjamin Thomas Blodgett <benvlodgi@gmail.com>
2016-07-07 02:00:10 -0400

    Add Matrix Math methods
    Great help from github.com/Metapyziks/VMFInstanceInserter

---
+++ BackstockPrecompiler/MatrixMath.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BackstockPrecompiler
+{
+    public class MatrixMath
+    {
+        public static decimal[,] Multiply3v3(decimal[,] matrix1, decimal[,] matrix2)
+        {
+            decimal[,] newMatrix = new decimal[3, 3];
+
+            for (int r = 0; r < 3; ++r)
+                for (int c = 0; c < 3; ++c)
+                    for (int i = 0; i < 3; ++i)
+                        newMatrix[r, c] += matrix1[r, i] * matrix2[i, c];
+
+            return newMatrix;
+        }
+
+        public static decimal[,] GetXRotation(decimal angle)
+        {
+            double angleRad = Convert.ToDouble(angle) * Math.PI / 180;
+            decimal cos = Convert.ToDecimal(Math.Cos(angleRad));
+            decimal sin = Convert.ToDecimal(Math.Sin(angleRad));
+
+            return new decimal[,] {
+                {  1M,  0M,  0M    }
+               ,{  0M,  cos, -sin  }
+               ,{  0M,  sin,  cos  }
+            };
+        }
+
+        public static decimal[,] GetYRotation(decimal angle)
+        {
+            double angleRad = Convert.ToDouble(angle) * Math.PI / 180;
+            decimal cos = Convert.ToDecimal(Math.Cos(angleRad));
+            decimal sin = Convert.ToDecimal(Math.Sin(angleRad));
+
+            return new decimal[,] {
+                {  cos,  0M,  sin  }
+               ,{  0M,   1M,  0M   }
+               ,{ -sin,  0M,  cos  }
+            };
+        }
+
+        public static decimal[,] GetZRotation(decimal angle)
+        {
+            double angleRad = Convert.ToDouble(angle) * Math.PI / 180;
+            decimal cos = Convert.ToDecimal(Math.Cos(angleRad));
+            decimal sin = Convert.ToDecimal(Math.Sin(angleRad));
+
+            return new decimal[,] {
+                {  cos, -sin,  0M  }
+               ,{  sin,  cos,  0M  }
+               ,{  0M,   0M,   1M  }
+            };
+        }
+
+        public static decimal[,] GetRotationMatrix(decimal Pitch, decimal Yaw, decimal Roll)
+        {
+            return MatrixMath.Multiply3v3(MatrixMath.GetZRotation(Yaw), MatrixMath.Multiply3v3(MatrixMath.GetYRotation(Pitch), MatrixMath.GetXRotation(Roll)));
+        }
+    }
+}
--- BackstockPrecompiler/Vector3.cs
+++ BackstockPrecompiler/Vector3.cs
@@ -31,5 +31,62 @@ namespace BackstockPrecompiler
         {
             return new Vector3() { X = X, Y = Y, Z = Z };
         }
+
+
+        public Vector3 GetTransformedPosition(Vector3 translation, Vector3 rotation)
+        {
+            var vec = Clone();
+            vec.Rotate(rotation);
+            vec.Offset(translation);
+            return vec;
+        }
+
+        public Vector3 GetTransformedRotation(Vector3 rotation)
+        {
+            var vec = Clone();
+            vec.Rotate(rotation);
+            return vec;
+        }
+
+        public Vector3 GetTransformedAngles(Vector3 rotation)
+        {
+            var vec = Clone();
+            vec.AddAngles(rotation);
+            return vec;
+        }
+
+        public void Rotate(Vector3 angles)
+        {
+            decimal[,] rotationMatrix = MatrixMath.GetRotationMatrix(angles.Y, angles.X, angles.Z);
+            decimal x = X, y = Y, z = Z;
+
+            X = x * rotationMatrix[0, 0] + y * rotationMatrix[0, 1] + z * rotationMatrix[0, 2];
+            Y = x * rotationMatrix[1, 0] + y * rotationMatrix[1, 1] + z * rotationMatrix[1, 2];
+            Z = x * rotationMatrix[2, 0] + y * rotationMatrix[2, 1] + z * rotationMatrix[2, 2];
+        }
+
+        public void AddAngles(Vector3 angles)
+        {
+            var a = MatrixMath.GetRotationMatrix(X, Y, Z);
+            var b = MatrixMath.GetRotationMatrix(angles.X, angles.Y, angles.Z);
+            
+            decimal[,] angleMatrix = MatrixMath.Multiply3v3(b, a);
+            double yaw = Math.Atan2((double)angleMatrix[1, 0], (double)angleMatrix[0, 0]) / Math.PI * 180.0;
+
+            angleMatrix = MatrixMath.Multiply3v3(MatrixMath.GetZRotation(Convert.ToDecimal(-yaw)), angleMatrix);
+            double pitch = Math.Atan2((double)-angleMatrix[2, 0], (double)angleMatrix[0, 0]) / Math.PI * 180.0;
+            double roll = Math.Atan2((double)-angleMatrix[1, 2], (double)angleMatrix[1, 1]) / Math.PI * 180.0;
+            
+            X = Convert.ToDecimal(pitch - Math.Floor(pitch / 360.0) * 360.0);
+            Y = Convert.ToDecimal(yaw - Math.Floor(yaw / 360.0) * 360.0);
+            Z = Convert.ToDecimal(roll - Math.Floor(roll / 360.0) * 360.0);
+        }
+
+        public void Offset(Vector3 vector)
+        {
+            X += vector.X;
+            Y += vector.Y;
+            Z += vector.Z;
+        }
     }
 }
